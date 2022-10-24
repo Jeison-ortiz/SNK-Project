612b9d22a011b61c93d2d9fcc5af680ecac1b6ca
Jeison-ortiz <ortizjeison4@gmial.com>
2022-10-24 14:48:36 -0500

    Se agrega estructura del proyecto

---
+++ src/main/java/com/SNKProject/SnkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.SNKProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnkProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SNKProject/SnkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SNKProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnkProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
